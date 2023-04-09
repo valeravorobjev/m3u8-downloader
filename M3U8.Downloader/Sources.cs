c6f2f5bf87074c45582c42043716a5c907d3775f
valerijvorobev <vorobjevvalera@gmail.com>
2023-04-09 13:53:31 +0300

    Separate classes

---
+++ M3U8.Downloader/Sources.cs
@@ -0,0 +1,27 @@
+namespace M3U8.Downloader;
+
+/// <summary>
+/// Class for source functions. Put here function that build video url(s).
+/// </summary>
+public static class Sources
+{
+    /// <summary>
+    /// If you need download video m3u8 from master file, use this function.
+    /// This function download and read master m3u8. Get one part and get it's items.
+    /// </summary>
+    /// <param name="url">Master m3u8 file</param>
+    /// <returns></returns>
+    public static async Task<IList<string>> MasterAsync(string url)
+    {
+        using HttpClient client = new HttpClient();
+        string content = await client.GetStringAsync(url);
+
+        var master = content.Split("\n").Last(c => c.Contains("https"));
+
+        content = await client.GetStringAsync(master);
+
+        var items = content.Split("\n").Where(c => c.Contains("https")).ToList();
+
+        return items;
+    }
+}
